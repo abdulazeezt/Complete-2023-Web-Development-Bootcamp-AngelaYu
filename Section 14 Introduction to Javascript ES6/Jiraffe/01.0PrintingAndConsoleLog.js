f8d4877fa84f64e4690775c2a21edbcc37faa4bd
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2025-02-01 21:52:34 +0530

    .
    .

---
+++ Section 14 Introduction to Javascript ES6/Jiraffe/01.0PrintingAndConsoleLog.js
@@ -0,0 +1,14 @@
+// alert("This is an alert Message")
+
+// alert(" <h1>  This is an H1 tag</h1>"     )
+
+
+document.write( "<h1>  This is an H1 tag</h1>"    )
+document.write("<br>")
+
+
+document.write( "<h1> This is the second tag  </h1>"    )
+
+
+
+console.log("a console log")
