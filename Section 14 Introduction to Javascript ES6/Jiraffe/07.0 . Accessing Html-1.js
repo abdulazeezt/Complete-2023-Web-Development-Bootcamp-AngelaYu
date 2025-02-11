1a05aee6ac7403c2acd5dd954332f719f22d56cd
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2025-02-11 08:51:41 +0530

    ..
    ..

---
+++ Section 14 Introduction to Javascript ES6/Jiraffe/07.0 . Accessing Html-1.js
@@ -0,0 +1,9 @@
+
+
+var header = document.getElementById("myHeader")
+// header.style = "Background"
+header.style="background-color: cadetblue; font-style: italic;"
+document.write(  "hello workd" )
+header.innerHTML  = " This is inner HTML"
+
+
---
+++ Section 14 Introduction to Javascript ES6/Jiraffe/08.0 Array.js
@@ -0,0 +1,13 @@
+// var headinG1 = document.getElementsByid("h1")
+// var heading1 = document.getElementById("h1")
+var heading1 = document.getElementsByTagName("h1")
+var luckynumbers= ["3","4","5","6","Hello", "true"]
+
+// len = luckynumbers.length
+// len = 100
+
+heading1[0].innerHTML = luckynumbers.length 
+heading1[1].innerHTML = luckynumbers[5]
+heading1[2].innerHTML = luckynumbers[4]
+heading1[3].innerHTML = "Three 4"
+
