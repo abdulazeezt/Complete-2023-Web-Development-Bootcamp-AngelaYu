889972d08754cf84ecdb584e38b85354dfaa4a8e
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2025-02-11 10:34:19 +0530

    .
    .

--- Section 14 Introduction to Javascript ES6/Jiraffe/09.0 Objects.js
+++ Section 14 Introduction to Javascript ES6/Jiraffe/09.0 Objects.js
@@ -1,3 +1,16 @@
-var mainhead = document.getElementById("mainheadh1")
 
-// mainhead.style = "color:blue; text"
+var student  = {
+  name: " abcd",  age:19,  gender: "Male",
+  Major:"Business",
+  gpa: 2.5
+};
+
+var h2a = document.getElementById("h2-1");
+// h2a.innerHTML = student.gpa;
+// document.write( h2_1.gender)
+// h2a.innerHTML("This is inner");
+h2a.innerText("ASFAFKAJSFA; ")
+// h2a.innerText("Hello world")
+h2a.value
+
+
