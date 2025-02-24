401f7bc4efc6ad207c02d8e475929b585f7b2c7c
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2025-02-24 21:29:31 +0530

    .
    .

--- Section 16 The Document Object Model - DOM/115/115 . Introduction To The Document Object Model - Dom.js
+++ Section 16 The Document Object Model - DOM/115/115 . Introduction To The Document Object Model - Dom.js
@@ -7,6 +7,7 @@ try {
 
     var h2error = document.getElementById("footerPara1")
     var h2Completion = document.getElementById("footerPara2")
+
     var flexparadiv1 = document.getElementsByClassName("box-1")
     var flexparadiv2 = document.getElementsByClassName("box-2")
 
@@ -15,7 +16,7 @@ try {
     mainpara1.innerHTML = tempstr
 
 function DocumentObject() {
-  // var temparr = new Array();
+  // // var temparr = new Array();
   var temparr ;
   // for (var i=0; i < 10 ; i++){
   //     var fib = i + prevval
@@ -24,37 +25,20 @@ function DocumentObject() {
   //     // console.log("Looping through : " +i +" , Previous val is: " +prevval +" , Current Fib is : " +fib )
   // }
 //  temparr =   document.firstElementChild
-
     // 1temparr = document.getElementsByTagName("li")
-
-  
-
-   
-
-
     return temparr
 
-
   }
-
-  var entirehtml = document.firstChild
-
+  var entirehtml = document.firstElementChild
   entirehtml = entirehtml.nodeValue
-
         mainpara1.innerHTML = "Returning Fib Array : <br> <br> " +entirehtml
-
       // mainpara1.innerHTML = "Returning Fib Array : <br> <br> " +DocumentObject().length
       // console.log("Returning Fib Array : <br> <br> " +DocumentObject()[0].name)
-
       // // document.write("<h4>Hello wold</h4>")
       // document.innerHTML = "<h4>Hello wold</h4>"
-
-
   //  entirehtml.firstChild.get
       console.log("Returning : Helloasdfas " +entirehtml   )
-
   //  ###################################################################################
-
   //  ###################################################################################
       h2error.innerHTML = " The code has been implemented and Successful."  
   //  ###################################################################################
